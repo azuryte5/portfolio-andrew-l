c963ba86cba56f201de674827a5af83831205f73
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2022-03-01 14:02:47 -0500

    fixed about page spacing, added background gradient and useMediaQuery

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,13 +5,13 @@ import { Link as Route} from "react-router-dom"
 
 function Nav() {
   return (
-    <Box p={3} bg="#413C58" color='#CDEDF6' zIndex={200} w="full" position="fixed">
-      <HStack>
-        <Link href="https://github.com/azuryte5" isExternal>Andrew Lefebvre</Link>
+    <Box p={3} bg="#413C58" color='#CDEDF6' zIndex={200} w="full" position="fixed" fontSize={30}>
+      <HStack p={1}>
+        <Link href="https://github.com/azuryte5" isExternal border='dashed' borderRadius='20px' p={1} >Andrew Lefebvre</Link>
         <Spacer />
-        <Link as={Route} to="/">About</Link>
-        <Link as={Route} to="/portfolio">Portfolio</Link>
-        <Link as={Route} to="/resume">Resume</Link>
+        <Link as={Route} to="/" border='dashed' borderRadius='20px' p={1}>About</Link>
+        <Link as={Route} to="/portfolio" border='dashed' borderRadius='20px' p={1}>Portfolio</Link>
+        <Link as={Route} to="/resume" border='dashed' borderRadius='20px' p={1}>Resume</Link>
       </HStack>
     </Box>
   );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,42 +1,62 @@
 import React from "react";
-import { Box, Wrap, Image, VStack } from "@chakra-ui/react"
+import { Text, Wrap, Image, VStack, WrapItem, useMediaQuery } from "@chakra-ui/react"
 
-const About = () => (
-  <Wrap color='#CDEDF6' bg='#034F4F' flexWrap p={20}>
-      <Box boxSize='sm' bg='#F8CAB4'>
+
+
+function About() {
+  const [isFullWidth] = useMediaQuery([
+    '(min-width: 1099px)'])
+
+  function determineWidth() {
+      if (isFullWidth) {
+        return '40%' } else return '90%'}
+
+  return (
+  <Wrap color='#CDEDF6'   
+  bgGradient={[
+    'linear(to-br, #413C58, #3DC4EC)',
+    'linear(to-bl, #413C58, #C6E0E8)',
+    'linear(to-t, #CDEDF6, #413C58)',
+  ]} 
+  spacing='30px' justify='center'
+  flexWrap paddingTop={24} paddingBottom={40}>
+      <WrapItem boxSize='sm' bg='#034F4F' borderRadius={25} boxShadow='dark-lg'>
         <Image
           alt="Man with cool classes"
           src="https://user-images.githubusercontent.com/85147307/149609585-ea0a0e3b-1bd0-4b0f-b862-e58514787bfd.jpg" 
-          borderRadius='full'
+          borderRadius='50rem'
           objectFit='contain'
           boxSize='sm'   
         />
-      </Box>
+      </WrapItem>
 
-    <Box bg='#413C58' fontSize={20} fontStyle='bold' p={2} h={20}>About Andrew</Box>
+    <WrapItem bg='#034F4F' color='#F8CAB4' fontSize={20} borderRadius={25} fontWeight='900' p={5} h={20} boxShadow='dark-lg'>About Andrew</WrapItem>
 
-    <VStack bg='#413C58' fontSize='1.2rem' w='40rem' boxSize='lg'>
-      <Box>
+    <WrapItem bg='#034F4F' color='#F8CAB4' fontSize='1.2rem' borderRadius={25} p={6} boxShadow='dark-lg' w={determineWidth}>
+      <VStack p={4}>
+      <Text fontSize='xl'>
         Full stack web developer with experience in implanting creative
         solutions to improve accessibility. With a previous background in
         working with people with disabilities to meet their life goals, I have
         persistence and dedication to my products and responsibilities.
-      </Box>
-      <Box>
+      </Text>
+      <Text fontSize='xl'>
         Recently earned a certificate in development from Carleton University
         where expanded on skills in JavaScript, React with MERN stack. I've
         successfully bridged my in-person problem solving skills to a digital
         landscape. I've been able to oversee, track, set goals and build on
         previous knowledge to incorporate new ideas.
-      </Box>
-      <Box>
+      </Text>
+      <Text fontSize='xl'>
         I excel at building tools that keep the user experience in mind so that
         progress matches end goal expectations. Promoting success by guiding,
         consulting, augmenting the capabilities of others to overcome obstacles
         and barriers with patience and dedication.
-      </Box>
-    </VStack>
+      </Text>
+      </VStack>
+    </WrapItem>
   </Wrap>
-);
+  ) 
+}
 
 export default About;
